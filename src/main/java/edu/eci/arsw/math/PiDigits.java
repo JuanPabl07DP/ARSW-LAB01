7290ed36aed4a559726b9c1cfeb5e9e8c2dd03d5
JuanPabl07DP <juanpadazer15@gmail.com>
2025-01-25 19:22:40 -0500

    Clase PiDigits funcional

--- src/main/java/edu/eci/arsw/math/PiDigits.java
+++ src/main/java/edu/eci/arsw/math/PiDigits.java
@@ -1,113 +1,114 @@
 package edu.eci.arsw.math;
 
-///  <summary>
-///  An implementation of the Bailey-Borwein-Plouffe formula for calculating hexadecimal
-///  digits of pi.
-///  https://en.wikipedia.org/wiki/Bailey%E2%80%93Borwein%E2%80%93Plouffe_formula
-///  *** Translated from C# code: https://github.com/mmoroney/DigitsOfPi ***
-///  </summary>
+import edu.eci.arsw.threads.PiDigitsThread;
+
+/**
+ *
+ * @author hcadavid
+ * @author Juan Pablo Daza Pereira
+ * @author Nicolas Bernal Fuquene
+ */
 public class PiDigits {
 
     private static int DigitsPerSum = 8;
     private static double Epsilon = 1e-17;
 
-    
     /**
      * Returns a range of hexadecimal digits of pi.
      * @param start The starting location of the range.
      * @param count The number of digits to return
      * @return An array containing the hexadecimal digits.
      */
-    public static byte[] getDigits(int start, int count) {
-        if (start < 0) {
+    public static byte[] getDigits(int start, int count, int N) throws InterruptedException {
+        if (start < 0 || count < 0 || N <= 0) {
             throw new RuntimeException("Invalid Interval");
         }
 
-        if (count < 0) {
-            throw new RuntimeException("Invalid Interval");
+        byte[] result = new byte[count];
+        PiDigitsThread[] threads = new PiDigitsThread[N];
+
+        int digitsPerThread = count / N;
+        int remainingDigits = count % N;
+        int currentStart = start;
+
+        // Creamos e iniciamos los hilos
+        for (int i = 0; i < N; i++) {
+            int threadDigits = digitsPerThread + (i == N - 1 ? remainingDigits : 0);
+            threads[i] = new PiDigitsThread(currentStart, threadDigits);
+            threads[i].start();
+            currentStart += threadDigits;
         }
 
+        //
+        int currentPosition = 0;
+        for (int i = 0; i < N; i++) {
+            threads[i].join();
+            byte[] threadDigits = threads[i].getDigits();
+            System.arraycopy(threadDigits, 0, result, currentPosition, threadDigits.length);
+            currentPosition += threadDigits.length;
+        }
+        return result;
+    }
+
+    public static byte[] getDigits(int start, int count){
+        if (start < 0 || count < 0) {
+            throw new RuntimeException("Invalid Interval");
+        }
         byte[] digits = new byte[count];
         double sum = 0;
 
         for (int i = 0; i < count; i++) {
-            if (i % DigitsPerSum == 0) {
-                sum = 4 * sum(1, start)
-                        - 2 * sum(4, start)
-                        - sum(5, start)
-                        - sum(6, start);
-
+            if (i % DigitsPerSum == 0){
+                sum = 4 * sum(1, start) - 2 * sum(4,start)
+                        - sum(5, start) - sum(6,start);
                 start += DigitsPerSum;
             }
-
             sum = 16 * (sum - Math.floor(sum));
             digits[i] = (byte) sum;
         }
-
         return digits;
     }
 
-    /// <summary>
-    /// Returns the sum of 16^(n - k)/(8 * k + m) from 0 to k.
-    /// </summary>
-    /// <param name="m"></param>
-    /// <param name="n"></param>
-    /// <returns></returns>
     private static double sum(int m, int n) {
         double sum = 0;
         int d = m;
         int power = n;
 
-        while (true) {
+        while (true){
             double term;
-
-            if (power > 0) {
+            if (power > 0){
                 term = (double) hexExponentModulo(power, d) / d;
-            } else {
-                term = Math.pow(16, power) / d;
-                if (term < Epsilon) {
+            }else {
+                term = Math.pow(16,power) / d;
+                if (term < Epsilon){
                     break;
                 }
             }
-
             sum += term;
             power--;
             d += 8;
         }
-
         return sum;
     }
 
-    /// <summary>
-    /// Return 16^p mod m.
-    /// </summary>
-    /// <param name="p"></param>
-    /// <param name="m"></param>
-    /// <returns></returns>
-    private static int hexExponentModulo(int p, int m) {
+    private static int hexExponentModulo(int p, int m){
         int power = 1;
-        while (power * 2 <= p) {
+        while (power * 2 <= p){
             power *= 2;
         }
-
         int result = 1;
-
-        while (power > 0) {
-            if (p >= power) {
+        while (power > 0){
+            if (p >= power){
                 result *= 16;
                 result %= m;
                 p -= power;
             }
-
             power /= 2;
-
-            if (power > 0) {
+            if (power > 0){
                 result *= result;
                 result %= m;
             }
         }
-
         return result;
     }
-
 }
